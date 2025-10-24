f7c42ea837d32c49c0265b8fb0324463586bd20c
dootsiedev <dootsiedev@gmail.com>
2025-10-24 14:45:59 -0400

    add better errors and start making TL output

--- main.cpp
+++ main.cpp
@@ -26,20 +26,78 @@ static llvm::cl::opt<std::string> ExtractFuncOption(
 	llvm::cl::Required,
 	llvm::cl::cat(MyToolCategory));
 
+static llvm::cl::opt<bool> Disable_SrcLoc(
+	"disable-srcloc",
+	llvm::cl::desc("disable source code references to the translations (may affect matching)"),
+	llvm::cl::Optional,
+	llvm::cl::cat(MyToolCategory),
+	llvm::cl::init(false));
+
+/*
+ // I will just use llvm::outs instead because I don't understand why llvm::errs wont be synced.
+ // As in, errs get printed either before or after outs (maybe this is the clion IDE's fault...)
 static llvm::cl::opt<std::string> OutputOption(
 	"o",
 	llvm::cl::desc("the output file, prints to stdout if not specified"),
 	llvm::cl::Optional,
 	llvm::cl::cat(MyToolCategory));
+*/
 
 using namespace clang;
 using namespace clang::tooling;
 
-// TODO: what if I renamed it to extract-strings? no need for the TL suffix because why not?
-// TODO: get comments
-// TODO: make a function that prints the output with source location and comments
-//		should it only be the macro format,
-//		or should I also support a easier to parse format like newline separated or json?
+void printPresumedLoc(clang::SourceManager& SM, clang::SourceLocation loc)
+{
+	// Check for an invalid location.
+	if(loc.isInvalid())
+	{
+		llvm::errs() << "<invalid loc>\n";
+		return;
+	}
+
+	// Get the presumed location, which accounts for #line directives.
+	clang::PresumedLoc presumed_loc = SM.getPresumedLoc(loc);
+
+	// Check for an invalid presumed location.
+	if(presumed_loc.isInvalid())
+	{
+		llvm::errs() << "<invalid presumed loc>\n";
+		return;
+	}
+
+	llvm::errs() << presumed_loc.getFilename() << ":" << presumed_loc.getLine() << ":"
+				 << presumed_loc.getColumn() << "\n";
+}
+
+//https://stackoverflow.com/questions/72571073/how-to-get-the-parent-funtion-name-in-clang-tidy-astmatch-at-callexpr-position
+#include "clang/AST/ParentMapContext.h"          // clang::DynTypedNodeList
+std::string get_caller_function(clang::ASTContext& Context, Expr* expr)
+{
+	// Get its parent nodes.  The docs do not realy explain why there can
+	// be multiple parents, but I think it has to do with C++ templates.
+	clang::DynTypedNodeList NodeList = Context.getParents(*expr);
+	while (!NodeList.empty())
+	{
+		// Get the first parent.
+		clang::DynTypedNode ParentNode = NodeList[0];
+
+		// You can dump the parent like this to inspect it.
+		// ParentNode.dump(llvm::outs(), *(Result.Context));
+
+		// Is the parent a FunctionDecl?
+		if(const FunctionDecl* Parent = ParentNode.get<FunctionDecl>())
+		{
+			//llvm::outs() << "Found ancestor FunctionDecl: " << (void const*)Parent << '\n';
+			//llvm::outs() << "FunctionDecl name: " << Parent->getNameAsString() << '\n';
+			return Parent->getNameAsString();
+		}
+
+		// It was not a FunctionDecl.  Keep going up.
+		NodeList = Context.getParents(ParentNode);
+	}
+	return std::string();
+}
+
 
 class PrintfStringASTVisitor : public RecursiveASTVisitor<PrintfStringASTVisitor>, CommentHandler
 {
@@ -65,7 +123,7 @@ public:
 			size_t pos = commentText.find("TRANSLATORS:");
 			if(pos != llvm::StringRef::npos)
 			{
-				llvm::outs() << "Found Comment:" << commentText << "\n";
+				//llvm::outs() << "Found Comment:" << commentText << "\n";
 				comments.emplace_back(commentText, Comment);
 			}
 		}
@@ -98,8 +156,9 @@ public:
 		{
 			if(!comment.used)
 			{
-				// TODO: ask AI if the library has a pretty formatter for errors with source location
+				// TODO: this could be a warning... since this is potentially in a macro...
 				llvm::errs() << "error: failed to match comment: "<< comment.text << "\n";
+				printPresumedLoc(SM, comment.range.getBegin());
 				success = false;
 			}
 		}
@@ -133,7 +192,7 @@ private:
 			escape_string(str, string);
 			string = str;
 		}
-		llvm::outs() << "Found string: \"" << string << "\"\n";
+		//llvm::outs() << "Found string: \"" << string << "\"\n";
 
 		// Get the comment for the function.
 		clang::SourceLocation loc = Arg->getExprLoc();
@@ -146,8 +205,22 @@ private:
 				{
 					break;
 				}
+				if(rit+1 != comments.rend())
+				{
+					if(!(rit+1)->used)
+					{
+						// TODO: this could be a warning... since this is potentially in a macro...
+						llvm::errs() << "error: comment not consumed!: "<< (rit+1)->text << "\n";
+						printPresumedLoc(SM, (rit+1)->range.getBegin());
+						llvm::errs() << "before here: "<< rit->text << "\n";
+						printPresumedLoc(SM, rit->range.getBegin());
+						return false;
+					}
+				}
+
 				// count the number of newlines.
 				// there has to be a better way...
+				// I could use presumed loc but I already wrote this.
 				llvm::StringRef gapText = clang::Lexer::getSourceText(
 					clang::CharSourceRange::getCharRange(rit->range.getBegin(), loc),
 					PP.getSourceManager(),
@@ -161,17 +234,53 @@ private:
 						// this is a hardcoded 2 line distance.
 						if(i >= 3)
 						{
-							// TODO: ask AI if the library has a pretty formatter for errors with source location
+							// TODO: this could be a warning... since this is potentially in a macro...
 							llvm::errs() << "error: gap between comment and translation too large: "<< rit->text << "\n";
+							printPresumedLoc(SM, rit->range.getBegin());
+							llvm::errs() << "to here: "<< string << "\n";
+							printPresumedLoc(SM, loc);
 							return false;
 						}
 					}
 				}
 				rit->used = true;
-				llvm::outs() << "Matched Comment: " << rit->text << "\n";
+
+				if(!Disable_SrcLoc)
+				{
+					if(loc.isInvalid())
+					{
+						llvm::errs() << "<invalid loc>\n";
+						return false;
+					}
+					// Get the presumed location, which accounts for #line directives.
+					clang::PresumedLoc presumed_loc = SM.getPresumedLoc(loc);
+
+					// Check for an invalid presumed location.
+					if(presumed_loc.isInvalid())
+					{
+						llvm::errs() << "<invalid presumed loc>\n";
+						return false;
+					}
+					// TODO: strip the whole path from the file,
+					//  but I really like how clion will find the path using the line+column...
+					llvm::outs() << "SRC_LOC(\"" << get_caller_function(Context, Arg) << "\", \""
+								 << presumed_loc.getFilename() << ":" << presumed_loc.getLine()
+								 << ":" << presumed_loc.getColumn() << "\", "
+								 << presumed_loc.getLine() << ")\n";
+				}
+
+				std::string escaped_comment_buf;
+				llvm::StringRef escaped_comment = rit->text;
+				if(escape_string_check_contains(rit->text))
+				{
+					escape_string(escaped_comment_buf, rit->text);
+					escaped_comment = escaped_comment_buf;
+				}
+				llvm::outs() << "COMMENT(\"" << escaped_comment << "\")\n";
 				break;
 			}
 		}
+		llvm::outs() << "TL(\"" << string << "\", NULL)\n\n";
 		return true;
 	}
 };
@@ -186,14 +295,18 @@ public:
 
 	void HandleTranslationUnit(ASTContext& Context) override
 	{
+		// I want all the files to be merged.
+		//llvm::outs() << "TL_START()\n\n";
 		if(!Visitor.TraverseDecl(Context.getTranslationUnitDecl()))
 		{
+			// I want the test to fail.
 			exit(1);
 		}
 		if(!Visitor.check_for_unmatched_comments())
 		{
 			exit(1);
 		}
+		//llvm::outs() << "TL_END()\n";
 	}
 
 private:
@@ -211,22 +324,20 @@ public:
 
 int main(int argc, const char** argv)
 {
-	std::vector<const char*> args(argv, argv + argc);
-	int args_size = args.size();
-	args.push_back("-fparse-all-comments");
-	// add "extra-arg" to change the flags, or try to modify the CompilationDatabase?
-	//
-	auto ExpectedParser = CommonOptionsParser::create(args_size, args.data(), MyToolCategory);
+	// wait... why do the comments work without this...?
+	// I probably should also disable warnings, but I think it's harmless ATM (performance?).
+	//args.push_back("--extra-arg=-fparse-all-comments");
+
+	auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
 	if(!ExpectedParser)
 	{
-		llvm::outs().flush();
 		llvm::errs() << ExpectedParser.takeError();
 		return 1;
 	}
 
 	if(!ExtractFuncOption.empty())
 	{
-		llvm::outs() << "--func value: " << ExtractFuncOption << "\n";
+		//llvm::outs() << "--func value: " << ExtractFuncOption << "\n";
 	}
 
 	CommonOptionsParser& OptionsParser = ExpectedParser.get();
--- test/test1.cpp
+++ test/test1.cpp
@@ -4,13 +4,13 @@ const char* _T(const char* c) { return c; }
 
 int main()
 {
-	printf(_T("test!\n"));
+	printf(_T("test!\n")); // T RANSLATORS: a broken comment!
 
 	printf(/* TRANSLATORS: a comment!*/ _T("test %s!\n"), "foo");
 
 	// TRANSLATORS: works (But not if you make the distance 1 more...)
 	printf(
-		_T("test\nnewline: %s!\n") // wontwork: a broken comment!
+		_T("test\nnewline: %s!\n")
 		,
 		"bar");
 
